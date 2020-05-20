07a9a88bbebc2c166e0b6709c89add7de2239f1a
yaljamal <yazan.aljamal1@gmail.com>
2020-05-21 01:02:33 +0300

    fix the lab

--- __tests__/input.test.js
+++ __tests__/input.test.js
@@ -1,16 +1,30 @@
 'use strict';
 const Input = require('../lib/input.js');
-jest.spyOn(global.console, 'log');
-describe('Inmput Module test',()=>{
-  it('does nothing when valid() is called with invalid options',()=>{
+const minmist=require('minimist');
+jest.mock('minimist');
+minmist.mockImplementationOnce(()=>({
+  add:'this is my first test',
+}));
+describe('input mudel',()=>{
+  it('return every thnges ',()=>{
     const input=new Input();
-    input.valid();
-    expect(console.log).toHaveBeenCalled();
+    expect(input).toEqual({
+      action:'add',
+      paylod:'the test',
+    });
   });
-  it('does nothing when valid() is called with valid options',()=>{
+  minmist.mockImplementationOnce(()=>({
+    wrong:'the first test',
+  }));
+  it('should throw the error invalid key',()=>{
     const input=new Input();
-    input.valid({action : 'add',payload:'some text'});
-    expect(console.log).toHaveBeenCalled();
+    expect(input).toEqual({});
   });
-  
+  minmist.mockImplementationOnce(()=>({
+    add:true,
+  }));
+  it('should throw the error invalid input',()=>{
+    const input=new Input();
+    expect(input).toEqual({});
+  });
 });
--- index.js
+++ index.js
@@ -1,8 +1,13 @@
 'use strict';
+require('dotenv').config();
 const Input = require('./lib/input.js');
 const Note = require('./lib/notes.js');
-
-const test = new Input();
-console.log(test.obj);
-
-const note = new Note(test.obj);
+const mongoose = require('mongoose');
+mongoose.connect(process.env.MONGODB_URL,{
+  useNewUrlParser:true,
+  useUnifiedTopology:true,
+});
+const input = new Input();
+const note = new Note(input.obj);
+note.execute(input)
+  .then(mongoose.disconnect());
