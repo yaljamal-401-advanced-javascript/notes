86ded4cbc72f00b9790e87245ad50f7611b1cac2
yaljamal <yazan.aljamal1@gmail.com>
2020-05-30 21:47:47 +0300

    fix list issue

--- DB/note-schema.js
+++ DB/note-schema.js
@@ -2,7 +2,7 @@
 const mongoose = require('mongoose');
 const note=new mongoose.Schema({
   text:{type:'string',require:true},
-  catogry:{type:'string',require:true},
+  category:{type:'string',require:true},
 
 });
 module.exports = mongoose.model('note', note);
